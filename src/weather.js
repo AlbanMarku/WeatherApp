2486c0215fcc74e9c5906181d35dd70299b1d56d
AlbanMarku <albanmarku@outlook.com>
2022-09-08 22:36:54 +0100

    cors check

--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,7 @@ const weather = (() => {
     async function getData(city) {
         try {
             const endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&id=524901&appid=9bee3cb739586729ce79627217fb845a&units=metric`;
-            const response = await fetch(endpoint);
+            const response = await fetch(endpoint,{ mode: "cors" });
             if (!response.ok) throw new Error(`City ${city} not found`);
             const processedData = await response.json();
             return convertData(processedData)
